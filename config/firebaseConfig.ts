784053ed99af32ec103b9995bec60e708469e895
Nick Gowler <ngowler@academic.rrc.ca>
2025-03-08 14:04:53 -0600

    Create firebase config.

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,7 +1,7 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
 import { getAuth, Auth } from "firebase-admin/auth";
-import serviceAccount from "../firebase-service-account.ci.json";
+import serviceAccount from "../loan-monitoring-system-d9975-firebase-adminsdk-fbsvc-2a7ce5cca3.json";
 
 initializeApp({
     credential: cert(serviceAccount as ServiceAccount),
